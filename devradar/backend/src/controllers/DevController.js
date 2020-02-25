38206f8d92bff22bbe259926f4e817ad46911a23
USUARIO <usuario@mini-de-usuario.cbnet.com.br>
2020-02-25 13:53:32 -0300

    Implementando camada de erros.

---
+++ devradar/backend/src/controllers/DevController.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+
+const Dev = require('../models/Dev');
+
+module.exports = {
+    index: async (req, res) => {
+
+        try {
+            const devs = await Dev.find();   
+            
+            return res.status(200).json(devs);
+        } catch (error) {
+            
+            return res.status(500).json(error.message);
+        }        
+    },
+    store: async (req, res) => {
+        const { github_username, techs, latitude, longitude } = req.body;        
+    
+        try {
+
+            let dev = await Dev.findOne({ github_username });            
+
+            if(!dev) {
+                const techsArray = techs.split(',').map(tech => tech.trim());
+    
+                const location = {
+                    type: 'Point',
+                    coordinates: [latitude, longitude]
+                };       
+                
+                const resGitHub = await axios.get(`https://api.github.com/users/${github_username}`);                
+    
+                const { name = login, avatar_url, bio } = resGitHub.data;
+        
+                dev = await Dev.create({
+                    github_username,
+                    name,
+                    avatar_url,
+                    bio,
+                    techs: techsArray,
+                    location
+                });                
+            }            
+
+            return res.status(200).json(dev);
+        } catch (error) {
+            return res.status(500).json(error);       
+        }
+    
+    }    
+};
--- devradar/backend/src/index.js
+++ devradar/backend/src/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const { handleError } = require('./helpers/error');
 const routes = require('./routes');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-uspyk.mongodb.net/devradar?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 
 app.use(express.json());
 app.use(routes);
+
+app.use((err, req, res, next) => {
+    handleError(err, res);
+});
 
 app.listen(3333);
