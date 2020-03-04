9eb02bdc6b41afc5c6d3e7454d05bf682268e5f9
USUARIO <usuario@mini-de-usuario.cbnet.com.br>
2020-03-04 11:21:01 -0300

    Estilização do frontend web.

--- devradar/web/src/App.js
+++ devradar/web/src/App.js
@@ -1,8 +1,59 @@
 import React from 'react';
 
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />          
+          </div>
+
+          
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>            
+          </div>
+
+          <button type="submit">Salvar</button>
+
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/3951364?s=460&v=4" alt="Vinícius Boaventura Dias" />
+              <div className="user-info">
+                <strong>Vinícius Boaventura Dias</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Experienced Javascript/ReactJS/React Native/Node.js/PHP Web Developer with a demonstrated history of working in the information technology and services industry</p>
+            <a href="https://github.com/vinbdias">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
   );
 }
 
